16ff9c1f221458013bd241c3b1024d057a423bbd
Alexander <apvannug-lhl-webdev@gmail.com>
2022-09-24 15:59:06 -0600

    added code to test for code assertion errors

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('👍👍👍 Assertion Passed: [actual] === [expected]');
+  } else {
+    console.log('👎👎👎 Assertion Failed: [actual] !== [expected]');
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); //Failed
+assertEqual(1, 1); //Passed
+assertEqual(15, 16); //Failed
+assertEqual('hello', 'goodbye'); //Failed
+assertEqual('works', 'works'); //Passed
