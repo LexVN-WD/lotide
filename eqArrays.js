d1dec26fa577ff21688d504e8476927fbbc02815
Alexander <apvannug-lhl-webdev@gmail.com>
2022-10-02 16:45:55 -0600

    created function to see if arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//(1): create function that takes in two arrays and returns try or false based on perfect match
+//(2): trying to compare the values of array1 and array2 if they are a match
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+      // if value at array1 index does not equal to value at array2 index, return false 
+    }
+  }
+  return true;
+  // loop through all options first to make sure we find something that is false FIRST, then return true if there are none
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
