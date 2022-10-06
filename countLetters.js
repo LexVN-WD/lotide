eba83e8bd9796edb0dd1476368484d7d7110eedc
Alexander <apvannug-lhl-webdev@gmail.com>
2022-10-06 13:00:54 -0600

    created function that counts how many letters are in a sentence or word

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+//ASSERT 
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//CODE
+//(1): take sentence as a string, return count of each of the letters in that sentence
+//(example): countLetters('LHL') should return L: 2 and H: 1
+
+const countLetters = (sentence) => {
+  const letters = sentence.split('');
+  //split the sentence into an array of letters
+  const results = {};
+  //let result be an empty object
+  for (letter of letters) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+countLetters("hello");
