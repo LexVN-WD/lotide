9542748d79b249ed38cb57c4d13b380406882ea9
Alexander <apvannug-lhl-webdev@gmail.com>
2022-10-14 22:49:58 -0600

    Refactore head.js

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   if (array === null) {
@@ -17,7 +10,4 @@ const head = function(array) {
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;
