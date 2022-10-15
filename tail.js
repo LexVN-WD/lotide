f0584635837f7b77213438efc48f8d8e491ee12e
Alexander <apvannug-lhl-webdev@gmail.com>
2022-10-14 22:55:03 -0600

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   if (array === null) {
@@ -20,10 +13,4 @@ const tail = function(array) {
   }
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result[1], "Labs");
+module.exports = tail;
