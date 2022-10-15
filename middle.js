c619ede4cde1bf35d5606ac2c9527dea48920f99
Alexander <apvannug-lhl-webdev@gmail.com>
2022-10-14 23:18:44 -0600

    Refactored middle.js

--- middle.js
+++ middle.js
@@ -1,25 +1,6 @@
 //TEST ASSERTIONS
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-      // if value at array1 index does not equal to value at array2 index, return false 
-    }
-  }
-  return true;
-  // loop through all options first to make sure we find something that is false FIRST, then return true if there are none
-};
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed:${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // MAIN CODE
 
@@ -43,12 +24,4 @@ const middle = function(arrays) {
   }
 };
 
-
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3]), 2);
+module.exports = middle;
