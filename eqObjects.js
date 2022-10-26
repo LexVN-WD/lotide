acec8629cc8b62f969adea709d3894e761fc3d04
Alexander <apvannug.lhl.webdev@gmail.com>
2022-10-25 22:20:04 -0600

    refactoring of eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,9 @@
-//Imports
+//IMPORTS
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
-//Code
+//REFACTORED CODE
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
@@ -31,22 +31,6 @@ const eqObjects = function(object1, object2) {
   // otherwise, return true
 };
 
-
-// eqArrays Function
-const eqArrays = function (array1, array2) {
-  if(array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-      // if value at array1 index does not equal to value at array2 index, return false 
-    }
-  }
-  return true;
-  // loop through all options first to make sure we find something that is false FIRST, then return true if there are none
-};
-
 // ORIGINAL CODE
  /*  const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
@@ -68,20 +52,5 @@ const eqArrays = function (array1, array2) {
       return result;
 }; */
 
-//TESTS (for later refactoring and stretch)
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// const cd3 = { c: "1", d: ["2", 3, 4] };
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// const abc = { a: "1", b: "2", c: "3" };
-// const cd = { c: "1", d: ["2", 3] };
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// const cd3 = { c: "1", d: ["2", 3, 4] };
-
-// console.log(eqObjects(ab, abc)); // => false
-// console.log(eqObjects(ab, ba)); // => true
-// console.log(eqObjects(cd, cd2)); // => false
-// console.log(eqObjects(cd3, cd2)); // => true
-// assertEqual(eqObjects(ab, ba), true); // => true
-// assertEqual(eqObjects(ab, abc), false); // => false*/
+//EXPORTS
+module.exports = eqObjects;
