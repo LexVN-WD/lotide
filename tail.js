e0f8549f52a02b19503ed21cfad7243e0888c330
Alexander <apvannug.lhl.webdev@gmail.com>
2022-10-25 22:36:18 -0600

    refactoring of tail.js

--- tail.js
+++ tail.js
@@ -1,5 +1,34 @@
+//IMPORTS
 const assertEqual = require('./assertEqual');
 
+// REFACTORED CODE
+const tail = function(array) {
+   let arrLength = array.length;
+   if (array === []) {
+    return [];
+   } else if (arrLength === 1) {
+    return [];
+   } else {
+    return array.slice(1);
+   }
+};
+
+// TESTS
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const words2 = ["hello"];
+// console.log(tail(words2));
+// assertEqual(words2.length, 1);
+
+// const words3 = [];
+// console.log(tail(words3));
+// assertEqual(words3.length, 0);
+
+
+
+/* ORIGINAL CODE
 const tail = function(array) {
   if (array === null) {
     // if the array is empty(null), return empty array
@@ -11,6 +40,6 @@ const tail = function(array) {
     return array.slice(1);
     // else, return array starting at index 1
   }
-};
+}; */
 
 module.exports = tail;
