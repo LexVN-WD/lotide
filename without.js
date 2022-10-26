f84ec732212c11dffe19d5092776513f148d983a
Alexander <apvannug.lhl.webdev@gmail.com>
2022-10-25 22:41:49 -0600

    refactoring of without.js

--- without.js
+++ without.js
@@ -1,27 +1,9 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-      // if value at array1 index does not equal to value at array2 index, return false
-    }
-  }
-  return true;
-  // loop through all options first to make sure we find something that is false FIRST, then return true if there are none
-};
+//IMPORTS
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed:${actual} !== ${expected}`);
-  }
-};
-
-
-
+// REFACTORED CODE
 const without = function(source, itemsToRemove) {
   let sourceEdit = [];
   for (let i = 0; i < source.length; i++) {
@@ -29,14 +11,15 @@ const without = function(source, itemsToRemove) {
       sourceEdit.push(source[i]);
     }
   }
-  console.log(sourceEdit);
+  return sourceEdit;
+  // changed this from console.log to return
 };
 
-//testing on  new computer
 
-// without([1, 2, 3], [1]); // => [2, 3]
 
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+// TESTS
+// console.log(without([1, 2, 3], [1])); // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 // const words = ["hello", "world", "lighthouse"];
 // without(words, ["lighthouse"]); // no need to capture return value for this test case
