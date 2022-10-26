3414d253d23f7c358b916e70ec4689191e186080
Alexander <apvannug.lhl.webdev@gmail.com>
2022-10-25 23:37:41 -0600

    refactoring of the test function

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,6 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
-
+const assertArraysEqual = require('../assertArraysEqual');
 //TEST
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true -> PASSED
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => false -> FAILED
-assertArraysEqual(eqArrays([1, 2, 4], [1, 2, 4]), true); // => true -> PASSED
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true -> PASSED
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // => false -> FAILED
+assertArraysEqual([1, 2, 4], [1, 2, 4]); // => true -> PASSED
